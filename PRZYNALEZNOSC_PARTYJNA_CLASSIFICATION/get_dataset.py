dcacdd3ca96ff2eb685d4c0e0c6a2612bf7a4911
Dominik Lech <dominiklechweb@gmail.com>
2025-05-11 20:08:13 +0200

    added getting members by club from the api

---
+++ PRZYNALEZNOSC_PARTYJNA_CLASSIFICATION/get_dataset.py
@@ -0,0 +1,22 @@
+import requests
+
+
+# we want to exclude deputies from unknown/noname clubs
+CLUBS_WHITELIST = ["Razem", "Lewica", "Polska2050-TD", "PSL-TD", "KO", "PiS", "Konfederacja"]
+
+DEPUTIES_URI = "https://api.sejm.gov.pl/sejm/term10/MP"
+
+response = requests.get(DEPUTIES_URI)
+response.raise_for_status()
+deputies = response.json()
+
+members_by_club = {club: [] for club in CLUBS_WHITELIST}
+for deputy in deputies:
+    club = deputy.get("club")
+    firstLastName = deputy.get("firstLastName")
+    id = deputy.get("id")
+    if club in CLUBS_WHITELIST and firstLastName and id:
+        info = {"firstLastName": firstLastName, "id": id}
+        members_by_club.setdefault(club, []).append(info)
+    
+print(members_by_club)
